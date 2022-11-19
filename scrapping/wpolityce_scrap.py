83f6a3b57f7c9e2000fc84cfd500f79c28a7c03b
LemurPwned <jakub.mojsiejuk@gmail.com>
2022-11-19 20:53:03 +0100

    Stopwords

--- scrapping/wpolityce_scrap.py
+++ scrapping/wpolityce_scrap.py
@@ -1,11 +1,13 @@
 import json
+import os
 
 import bs4
 import requests
+from tqdm import tqdm
 
 
-def read_page():
-    with open("scrapping/wpolityce/Nbp.html") as f:
+def read_page(src_name: str):
+    with open(f"data/wpolityce/{src_name}.html") as f:
         return f.read()
 
 
@@ -17,11 +19,13 @@ def fetch_article_wgospodarce(link: str):
         article_soup = bs4.BeautifulSoup(article_text, "html.parser")
         title = article_soup.find("h2", class_="publication__title").get_text()
         article["title"] = title
-        date = article_soup.find("li", class_="meta__item meta__item--dates").get_text()
+        date = article_soup.find(
+            "li", class_="meta__item meta__item--dates").get_text()
         article["date"] = date
         author = article_soup.find("p", class_="author__name").get_text()
         article["author"] = author
-        content = article_soup.find("main", class_="publication__body").get_text()
+        content = article_soup.find("main",
+                                    class_="publication__body").get_text()
         article["content"] = content
     return article
 
@@ -36,44 +40,41 @@ def fetch_article_wpolityce(link: str):
         article["title"] = title
         date = article_soup.find("time", class_="js-relative-date")["title"]
         article["date"] = date
-        author = article_soup.find("h3", class_="article__author-name").get_text()
+        author = article_soup.find("h3",
+                                   class_="article__author-name").get_text()
         article["author"] = author
-        content = article_soup.find("section", class_="article__main").get_text()
+        content = article_soup.find("section",
+                                    class_="article__main").get_text()
         article["content"] = content
     return article
 
 
-def scrap():
-    data = []
-    for article_link in get_article_links():
-        if "wgospodarce" in article_link:
-            try:
+def scrap(src_name: str):
+    indx = 0
+    links = get_article_links(src_name)
+    for article_link in tqdm():
+        save_path = f"data/wpolityce_articles/{src_name.lower()}_{indx}.json"
+        if os.path.exists(save_path):
+            continue
+        try:
+            if "wgospodarce" in article_link:
                 content = fetch_article_wgospodarce(article_link)
-            except:
-                pass
-            else:
-                data.append(content)
-        elif "wpolityce" in article_link:
-            try:
+            elif "wpolityce" in article_link:
                 content = fetch_article_wpolityce(article_link)
-            except:
-                pass
-            else:
-                data.append(content)
-    json.dump(
-        data,
-        open("scrapping/wpolityce/data.json", mode="w"),
-        indent=4,
-        sort_keys=True,
-    )
+        except:
+            continue
+        json.dump(content, open(save_path, "w"), indent=True, sort_keys=True)
+        indx += 1
 
 
-def get_article_links():
-    page_html = read_page()
+def get_article_links(src_name: str):
+    page_html = read_page(src_name)
     page_soup = bs4.BeautifulSoup(page_html, "html.parser")
+    print("Scraping links from", src_name)
     for anchor in page_soup.find_all("a", class_="tile__link"):
         yield anchor["href"]
 
 
 if __name__ == "__main__":
-    scrap()
+    for src_name in ["Gospodarka", "NBP"]:
+        scrap(src_name)
