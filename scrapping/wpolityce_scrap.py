566610673684ad17c2dbf50e074bfba0bac6d394
LemurPwned <jakub.mojsiejuk@gmail.com>
2022-11-19 21:41:00 +0100

    wpolityce + DB

--- scrapping/wpolityce_scrap.py
+++ scrapping/wpolityce_scrap.py
@@ -5,6 +5,8 @@ import bs4
 import requests
 from tqdm import tqdm
 
+SRC_PAGE = "https://wpolityce.pl"
+
 
 def read_page(src_name: str):
     with open(f"data/wpolityce/{src_name}.html") as f:
@@ -45,36 +47,64 @@ def fetch_article_wpolityce(link: str):
         article["author"] = author
         content = article_soup.find("section",
                                     class_="article__main").get_text()
-        article["content"] = content
+        article["content"] = clean_text(content)
     return article
+
+
+def clean_text(text):
+    """Clean text"""
+    text = text.replace("\n", " ")
+    text = text.replace("\t", " ")
+    text = text.replace("\r", " ")
+    text = text.replace("  ", " ")
+    text = text.replace("   ", " ")
+    text = text.replace("    ", " ")
+    return text
 
 
 def scrap(src_name: str):
     indx = 0
     links = get_article_links(src_name)
-    for article_link in tqdm():
+    for article_link in tqdm(links):
         save_path = f"data/wpolityce_articles/{src_name.lower()}_{indx}.json"
+        if article_link.startswith('/'):
+            article_link = SRC_PAGE + article_link
+        content = ''
         if os.path.exists(save_path):
+            indx += 1
             continue
         try:
             if "wgospodarce" in article_link:
                 content = fetch_article_wgospodarce(article_link)
             elif "wpolityce" in article_link:
                 content = fetch_article_wpolityce(article_link)
-        except:
+        except (AttributeError, requests.exceptions.ConnectionError,
+                requests.exceptions.TooManyRedirects,
+                requests.exceptions.ReadTimeout, FileNotFoundError) as ex:
+            print("Error: {}".format(ex))
+            indx += 1
             continue
-        json.dump(content, open(save_path, "w"), indent=True, sort_keys=True)
+        if content:
+            json.dump(content,
+                      open(save_path, "w"),
+                      indent=True,
+                      sort_keys=True)
         indx += 1
 
 
 def get_article_links(src_name: str):
     page_html = read_page(src_name)
-    page_soup = bs4.BeautifulSoup(page_html, "html.parser")
+    page_soup = bs4.BeautifulSoup(page_html, "lxml")
     print("Scraping links from", src_name)
-    for anchor in page_soup.find_all("a", class_="tile__link"):
-        yield anchor["href"]
+    links = page_soup.find_all("a", class_="tile__link")
+    return [l.get("href") for l in links]
 
 
 if __name__ == "__main__":
-    for src_name in ["Gospodarka", "NBP"]:
+
+    # page = read_page('Gospodarka')
+    # print("Read")
+    # soup = bs4.BeautifulSoup(page, 'lxml')
+
+    for src_name in ["Gospodarka", "Nbp"]:
         scrap(src_name)
